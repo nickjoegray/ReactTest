d6b61e0a10bcb48528a4a661158f9707ec086ad8
Nick Gray <nick.gray@rci.rogers.ca>
2016-01-28 11:01:56 -0500

    Setup React dev environment

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// Compile JS and JSX down to JS and start the dev server
+
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: './',
+		filename: 'index.js'
+	},
+	devServer: {
+		inline: true,
+		port: 3333
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+}
